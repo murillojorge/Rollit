3e209d69011fc1229757cd52510db4bf584cccd9
Jorge Murillo <murillojorge@gmail.com>
2016-02-04 22:02:54 -0600

    regexp

--- rollit.rb
+++ rollit.rb
@@ -1,10 +1,11 @@
 
-
-ammount = 5
-faces = 10
-
-ammount.times do |i|
-	rollit = rand(faces)
-	puts rollit
-end
-
+  
+  roll = "1d6"
+  /(?<amount>)\d(?<faces>)/ =~ roll
+  amount.to_i
+  faces.to_i
+  amount.times do |i|
+    rollit = rand(faces)
+    puts rollit
+  end
+  
