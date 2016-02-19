f83948957d795d112c28b5edf29e32d35f61d18a
Jorge Murillo <murillojorge@gmail.com>
2016-02-18 18:24:44 -0600

    Working regex

--- rollit.rb
+++ rollit.rb
@@ -1,7 +1,8 @@
 
-  roll = "1d6"
-  /(?<amount>)\d(?<faces>)/ =~ roll
-
+  roll = "2d6"
+  match = /(?<a>\d+)[d](?<f>\d+)/.match(roll)
+  amount = match[1].to_i
+  faces = match[2].to_i
   amount.times do |i|
     rollit = rand(faces)
     puts rollit
