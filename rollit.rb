608dd4abae25f013a841877cecec1c2c8e0ddf1c
Jorge Murillo <murillojorge@gmail.com>
2016-02-19 17:46:09 -0600

    no zero face and sum class

--- rollit.rb
+++ rollit.rb
@@ -1,10 +1,12 @@
-
-  roll = "2d6"
-  match = /(?<a>\d+)[d](?<f>\d+)/.match(roll)
-  amount = match[1].to_i
-  faces = match[2].to_i
-  amount.times do |i|
-    rollit = rand(faces)
-    puts rollit
-  end
-  
+class Array
+    def sum
+        self.inject{|memo,n| memo + n }
+    end
+end
+dice = "3d20"
+match = /(?<a>\d+)[d](?<f>\d+)/.match(dice)
+amount = match[1].to_i
+faces = match[2].to_i  
+roll = amount.times.collect { rand(1..faces) }
+puts roll.inspect
+puts roll.sum
