6c72d773816157b2352893af97d8602b5310fdbe
Jorge Murillo <murillojorge@gmail.com>
2016-02-22 17:21:10 -0600

    Def roll puts

--- rollit.rb
+++ rollit.rb
@@ -3,10 +3,12 @@ class Array
         self.inject{|memo,n| memo + n }
     end
 end
-dice = "3d20"
-match = /(?<a>\d+)[d](?<f>\d+)/.match(dice)
-amount = match[1].to_i
-faces = match[2].to_i  
-roll = amount.times.collect { rand(1..faces) }
-puts roll.inspect
-puts roll.sum
+
+def roll
+	dice = "2d100"
+	match = /(?<a>\d+)[d](?<f>\d+)/.match(dice)
+	amount = match[1].to_i
+	faces = match[2].to_i  
+	roll = amount.times.collect { rand(1..faces) }
+	puts = 'Rolled ' + dice + ' and results are: ' + roll.to_s + ' | Total is: ' + roll.sum.to_s
+end
