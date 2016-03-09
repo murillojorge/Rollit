25bc143d7c75dfbd9321f41f9cc72d253b9ab3fb
Jorge Murillo <murillojorge@gmail.com>
2016-03-08 18:10:01 -0600

    Figuring instance variables

--- rollit.rb
+++ rollit.rb
@@ -8,14 +8,14 @@ class Rollit
 	
 	attr_reader :dice, :amount, :faces
 	
-	def initialize(dice)
+	def initialize(dice, amount, faces)
 		@dice = dice
+		@amount = amount
+		@faces = faces
 	end
 	
 	def regex
-		/(?<a>\d+)[d](?<f>\d+)/.match(dice)
-		@amount = amount
-		@faces = faces
+		/(?<amount>\d+)[d](?<faces>\d+)/.match(dice)
 		amount = match[1].to_i
 		faces = match[2].to_i 
 	end
@@ -23,7 +23,8 @@ class Rollit
 	def roll
     	amount.times.collect { rand(1..faces) }
   	end
+	
 end
 
-#puts = 'Rolled ' + @dice + ' and results are: ' + roll + ' | Total is: ' + roll
+#puts = 'Rolled ' + dice + ' and results are: ' + roll.to_s + ' | Total is: ' + roll.to_s
 
